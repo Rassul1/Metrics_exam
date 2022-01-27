f946384572db81bcce70d9d0827b7bab5008339c
Rassul1 <72034424+Rassul1@users.noreply.github.com>
2022-01-27 22:27:46 +0500

    commit1

--- exam1.py
+++ exam1.py
@@ -1,9 +1,34 @@
 import unittest
 import math
-def degree(x):
-    print(x)
-class TestStringMethods(unittest.TestCase):
+
+more = "More zero"
+inv = "Invalid"
+less = "Less zero"
+
+def results(x):
+    print("Input:",x)
+    result = ""
+    if type(x) == float:
+        num = math.gamma(x)
+        if num> 0:
+            result = more
+        else:
+            result = less
+    else:
+        result = inv
+    print(result,"\n")
+    return result
 
 
-if __name__ == '__main__':
+class TestDevisionResults(unittest.TestCase):
+    def test_MoreThatZero(self):
+        self.assertEqual(results(8), more)
+
+    def test_LessThatZero(self):
+        self.assertEqual(results(-0.1), less)
+
+    def test_Invalid(self):
+        self.assertEqual(results("d"), inv)
+
+if __name__ == 'main':
     unittest.main()
